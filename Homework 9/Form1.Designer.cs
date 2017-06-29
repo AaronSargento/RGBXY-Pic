9676e540c216beb9cf45fff55b956ad173eafddc
AaronSargento <aaron.sargento@yahoo.com>
2017-06-28 19:43:39 -0700

    First Commit

---
+++ Homework 9/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Homework_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadPictureBox = new System.Windows.Forms.PictureBox();
+            this.locationListBox = new System.Windows.Forms.ListBox();
+            this.colorListBox = new System.Windows.Forms.ListBox();
+            this.loadPictureButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.locationLabel = new System.Windows.Forms.Label();
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.promptLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.loadPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadPictureBox
+            // 
+            this.loadPictureBox.Location = new System.Drawing.Point(423, 92);
+            this.loadPictureBox.Name = "loadPictureBox";
+            this.loadPictureBox.Size = new System.Drawing.Size(600, 454);
+            this.loadPictureBox.TabIndex = 0;
+            this.loadPictureBox.TabStop = false;
+            this.loadPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.loadPictureBox_MouseClick);
+            this.loadPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.loadPictureBox_MouseMove);
+            // 
+            // locationListBox
+            // 
+            this.locationListBox.FormattingEnabled = true;
+            this.locationListBox.Location = new System.Drawing.Point(12, 108);
+            this.locationListBox.Name = "locationListBox";
+            this.locationListBox.Size = new System.Drawing.Size(229, 147);
+            this.locationListBox.TabIndex = 1;
+            // 
+            // colorListBox
+            // 
+            this.colorListBox.FormattingEnabled = true;
+            this.colorListBox.Location = new System.Drawing.Point(12, 284);
+            this.colorListBox.Name = "colorListBox";
+            this.colorListBox.Size = new System.Drawing.Size(229, 147);
+            this.colorListBox.TabIndex = 2;
+            // 
+            // loadPictureButton
+            // 
+            this.loadPictureButton.Location = new System.Drawing.Point(12, 50);
+            this.loadPictureButton.Name = "loadPictureButton";
+            this.loadPictureButton.Size = new System.Drawing.Size(75, 23);
+            this.loadPictureButton.TabIndex = 3;
+            this.loadPictureButton.Text = "Load Picture";
+            this.loadPictureButton.UseVisualStyleBackColor = true;
+            this.loadPictureButton.Click += new System.EventHandler(this.loadPictureButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(12, 469);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // locationLabel
+            // 
+            this.locationLabel.AutoSize = true;
+            this.locationLabel.Location = new System.Drawing.Point(9, 92);
+            this.locationLabel.Name = "locationLabel";
+            this.locationLabel.Size = new System.Drawing.Size(167, 13);
+            this.locationLabel.TabIndex = 5;
+            this.locationLabel.Text = "Location of current pixel of picture";
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.Location = new System.Drawing.Point(12, 268);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(150, 13);
+            this.colorLabel.TabIndex = 6;
+            this.colorLabel.Text = "Color of current pixel of picture";
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promptLabel.Location = new System.Drawing.Point(419, 69);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(210, 20);
+            this.promptLabel.TabIndex = 7;
+            this.promptLabel.Text = "Load Picture into Application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 558);
+            this.Controls.Add(this.promptLabel);
+            this.Controls.Add(this.colorLabel);
+            this.Controls.Add(this.locationLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loadPictureButton);
+            this.Controls.Add(this.colorListBox);
+            this.Controls.Add(this.locationListBox);
+            this.Controls.Add(this.loadPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.loadPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox loadPictureBox;
+        private System.Windows.Forms.ListBox locationListBox;
+        private System.Windows.Forms.ListBox colorListBox;
+        private System.Windows.Forms.Button loadPictureButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label locationLabel;
+        private System.Windows.Forms.Label colorLabel;
+        private System.Windows.Forms.Label promptLabel;
+    }
+}
+
---
+++ Homework 9/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Input: The user can click 'Load Picture' to load a picture onto the application. The user can click 'Exit' to exit out of application.
+// Output: Once the picture is loaded, if the user clicks on any part of the picture box, the location (x,y) and color (r,g,b) will be given of the current pixel
+
+namespace Homework_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            This function allows the user to exit out of the application
+        */
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /*
+            This function will allow the user to grab a picture from their library
+        */
+        private void loadPictureButton_Click(object sender, EventArgs e)
+        {
+            //Clear the boxes each time we want to load a picture
+            locationListBox.Items.Clear();
+            colorListBox.Items.Clear();
+
+            OpenFileDialog image1 = new OpenFileDialog();
+            image1.Filter = "Image Files(*.jpg;*.jpeg;*.gif;*.bmp;*.png)|*.jpg;*.jpeg;*.gif;*.bmp;*.png";
+
+            if (image1.ShowDialog() == DialogResult.OK)
+            {
+                loadPictureBox.Image = new Bitmap(image1.FileName);
+                loadPictureBox.Size = loadPictureBox.Image.Size;
+                promptLabel.Text = "Move mouse to get current Pixel Location and Color";
+            }
+        }
+
+        /*
+            This function will allow the user to get the color and location of the pixel clicked on the picture box.
+        */
+        private void loadPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Clear the List Boxes for each iteration
+            locationListBox.Items.Clear();
+            colorListBox.Items.Clear();
+
+            try
+            {
+                //Add titles
+                locationListBox.Items.Add("Pixel Location (X, Y)");
+                colorListBox.Items.Add("Pixel Color (R, G, B)");
+
+                Bitmap image2 = new Bitmap(loadPictureBox.Image);
+                Color pixelColor = image2.GetPixel(e.X, e.Y);
+                locationListBox.Items.Add("X:" + e.X.ToString() + ", Y: " + e.Y.ToString());
+                colorListBox.Items.Add("R: " + pixelColor.R.ToString() + ", G: " + pixelColor.G.ToString() + ", B: " + pixelColor.B.ToString());
+
+                promptLabel.Text = "Successfully solidified current pixel location and color";
+                MessageBox.Show(" Success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /*
+            This function will allow the user to get the color and location of the current pixel (on cursor) on the picture box.
+        */
+        private void loadPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (loadPictureBox.Image != null)
+            {
+                promptLabel.Text = "Click mouse to get current Pixel Location and Color";
+                locationListBox.Items.Insert(0, "Pixel Location (X, Y)");
+                colorListBox.Items.Insert(0, "Pixel Color (R, G, B)");
+
+                try
+                {
+                    Bitmap image2 = new Bitmap(loadPictureBox.Image);
+                    Color pixelColor = image2.GetPixel(e.X, e.Y);
+
+                    //Place current location at top of list
+                    locationListBox.Items.Insert(1, "X:" + e.X.ToString() + ", Y: " + e.Y.ToString());
+                    colorListBox.Items.Insert(1, "R: " + pixelColor.R.ToString() + ", G: " + pixelColor.G.ToString() + ", B: " + pixelColor.B.ToString());
+
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
